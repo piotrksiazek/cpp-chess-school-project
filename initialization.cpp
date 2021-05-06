a7347878cf7f4b4c7b00dd91d5bff82ad7c73002
Piotr <pksiazek.py@gmail.com>
2021-05-06 13:00:14 +0200

    initial commit

---
+++ initialization.cpp
@@ -0,0 +1,42 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+SDL_Window* createWindow(int width, int height, const char* title)
+{
+    /*Tworzę okno*/
+    SDL_Window* win = SDL_CreateWindow(title,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED,
+                                        width, height, 0);
+    
+    /*Sprawdzam, czy okno zostało stworzone*/
+    if(!win)
+    {
+        cout<<"Window creation error"<<endl;
+        SDL_Quit();
+        exit(1);
+    }
+    return win;
+}
+
+int initSDL()
+{
+    /*Inicjalizuję bibliotekę SDL*/
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        cout<<"SDL initialization error"<<endl;
+        SDL_Quit();
+        return 1;
+    }
+    cout<<"success"<<endl;
+    return 0;
+}
+
+int main()
+{
+    initSDL();
+    SDL_Window*win = createWindow(500, 500, "title"); /*tworzę okno*/
+    SDL_Renderer* renderer = SDL_CreateRenderer( win, -1, SDL_RENDERER_ACCELERATED); /*tworzę renderer*/
+}
