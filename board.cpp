5e93cdc0dcd085dfd0de9bee2ef1441cc70a7fe4
piotr <pksiazek.py@gmail.com>
2021-05-07 22:18:53 +0200

    fix Piece.render() method

--- board.cpp
+++ board.cpp
@@ -25,13 +25,9 @@ void Board::initBoard()
             if(++counter % 2 == 0)
             {
                 this->board[y][x].addProps(x, y, WHITE, this->renderer);
-                King king(x, y, "king", "king.png", this->renderer);
-                // this->board[y][x].piece = king;
-                // this->board[y][x].piece->render(&this->board[y][x].rectangle);
-                SDL_Surface *image;
-                image = IMG_Load("king.png");
-                SDL_Texture* image_texture = SDL_CreateTextureFromSurface(renderer, image);
-                SDL_RenderCopy(this->renderer, king.image_texture, nullptr, &this->board[y][x].rectangle);
+                Piece *king = new King(x, y, "king", "king.png", this->renderer);
+                this->board[y][x].piece = king;
+                this->board[y][x].piece->render(&this->board[y][x].rectangle);
             }
             else
                 this->board[y][x].addProps(x, y, BLACK, this->renderer);
